a06921db8bd1581c005f9527d7cd3ce70c8d0683
gaowanyi <44829326+gaowanyi@users.noreply.github.com>
2018-11-15 22:57:51 +0800

    Add files via upload

---
+++ liulili_user_equip.sql
@@ -0,0 +1,11 @@
+use shopping;
+-- insert into user(user_name,user_password) values('user1','111111'),('user2','222222');
+-- select * from user;
+-- delete from user where user_id=3;
+-- select * from user;
+-- delete from user where user_id=4;
+-- select * from user;
+-- insert into equip(equip_name,equip_money,user_id) values
+-- ('equip1',1000,1),('equip11',2000,1),('equip2',3000,2);
+-- update user set user_id=1000 where user_id=1;
+select * from equip;
